684562e7745b4179d7c1ba76caf7492ceb949876
Madalena Silva <madalenarssilva@gmail.com>
2019-12-05 17:38:33 +0000

    Testar merge

--- src/Cisuc.java
+++ src/Cisuc.java
@@ -38,7 +38,7 @@ class Cisuc {
         // Ask for username and password
         Scanner scanner = new Scanner(System.in);
         System.out.println();
-        System.out.print("Username: ");
+        System.out.print("Ufgdgsername: ");
         String nome = scanner.nextLine();
         System.out.print("Password: ");
         String password = scanner.nextLine();
