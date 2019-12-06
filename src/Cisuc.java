7b9a67fbde05f9a45cff4863d64ec7b8d0866bcb
Inês <Inesbt.8@gmail.com>
2019-12-06 15:11:37 +0000

    Merge branch 'master' of C:\Users\Utilizador\Desktop\Cisuc with conflicts.

--- src/Cisuc.java
+++ src/Cisuc.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author Madalena Silva
  */
 
-public class Cisuc {
+class Cisuc {
 
     private ArrayList<Pessoa> pessoas;
     private ArrayList<Projeto> projetos;
@@ -29,11 +29,12 @@ public class Cisuc {
      * The Constructor has the Menu method
      */
 
-    Cisuc() {
+    Cisuc(){
 
         System.out.println("-----------Inicio----------");
         pessoas = new ArrayList<Pessoa>();
         projetos = new ArrayList<Projeto>();
+
         // Ask for username and password
         Scanner scanner = new Scanner(System.in);
         System.out.println();
@@ -43,19 +44,16 @@ public class Cisuc {
         String password = scanner.nextLine();
 
         //Menu
-        while (!exitMenu) {
+        while(!exitMenu){
             Menu();
             if (!exitMenu) {
                 Menu();
             }
         }
         System.exit(0);
-
-        System.out.println("-----------Fim----------");
-
     }
 
-    public void Menu() {
+    public void Menu(){
 
         /**
          * This menu is only a representation of our future graphic interface.
@@ -66,9 +64,7 @@ public class Cisuc {
         Scanner scanner = new Scanner(System.in);
         System.out.println();
         System.out.println("Menu: ");
-        System.out.println("1. Criar projetos.");
-        System.out.println("2. Adicionar pessoas à app.");
-        System.out.println("1. Associar pessoas a projetos.");
+        System.out.println("1. Criar projetos e associar pessoas.");
         System.out.println("2. Listar os projetos não concluídos na data estimada.");
         System.out.println("3. Listar os projectos concluídos.");
         System.out.println("4. Editar Projeto");
@@ -82,8 +78,7 @@ public class Cisuc {
                 printProjetos();
                 break;
             case 2:
-                criarPessoas();
-                printPessoas();
+                System.out.println("2");
                 break;
             case 3:
                 System.out.println("3");
@@ -102,7 +97,6 @@ public class Cisuc {
 
     /**
      * Get Nome
-     *
      * @return nome
      */
     public String getNome() {
@@ -111,7 +105,6 @@ public class Cisuc {
 
     /**
      * Set Nome
-     *
      * @param nome
      */
 
@@ -119,9 +112,7 @@ public class Cisuc {
         this.nome = nome;
     }
 
-    /**
-     * Get Password
-     *
+    /** Get Password
      * @return password
      */
     public String getPassword() {
@@ -130,30 +121,29 @@ public class Cisuc {
 
     /**
      * Set Password
-     *
      * @param password
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    //                                           FICHEIROS
 
     public ArrayList<Pessoa> lerFicheirosPessoas() {
 
         /**
-         * Method to read people info from file and use it to create instances of object Pessoa.
+         * Method to read people's info from file and use it to create instances of object Pessoa.
          * Docentes appear in the top of the file so that we can get Licenciados' and Mestres' orientadores.
          * If object file exists, that is the file that's read. Otherwise, text file is read.
          *
          * @return ArrayList of the objects Pessoa of people read from file.
          */
 
-        //Array para guardar todas as pessoas lidas do ficheiro.
+        //Array para guardar todas as pessoas lidas do ficheiro (de texto ou de objetos).
         ArrayList<Pessoa> pessoas_lidas = new ArrayList<>();
         File fich_objetos = new File("pessoas");
         File fich_texto = new File("pessoas.txt");
 
+        //Tentar ler ficheiro de objetos.
         try {
             FileInputStream f = new FileInputStream(fich_objetos);
             ObjectInputStream o = new ObjectInputStream(f);
@@ -231,9 +221,18 @@ public class Cisuc {
         return pessoas_lidas;
     }
 
-    //                                          CRIAR PROJETOS E PESSOAS
+    public ArrayList<Projeto> lerFicheirosProjetos() {
+        
+        /**
+         * Method to read people's info from file and use it to create instances of object Pessoa.
+         * Docentes appear in the top of the file so that we can get Licenciados' and Mestres' orientadores.
+         * If object file exists, that is the file that's read. Otherwise, text file is read.
+         *
+         * @return ArrayList of the objects Pessoa of people read from file.
+         */
+    }
 
-    public void criarProjetos() {
+    public void criarProjetos(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("-----Criação do Projeto------");
         System.out.println("Nome: ");
@@ -245,159 +244,19 @@ public class Cisuc {
         System.out.println("DataFim: ");
         String dataFimP = scanner.nextLine();
 
-        Projeto p = new Projeto(nome, acronimo, dataInicioP, dataFimP);
+        Projeto p = new Projeto(nome, acronimo, dataInicioP,dataFimP);
+        addProjeto(p);
+    }
+
+    public void addProjeto(Projeto p){
         projetos.add(p);
     }
 
-    public void printProjetos() {
-        for (Projeto p : projetos) {
+    public void printProjetos(){
+        for (Projeto p: projetos){
             System.out.println(p);
         }
     }
 
-    public void criarPessoas() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("-----Criação Pessoas------");
-        System.out.println("Nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Mail: ");
-        String mail = scanner.nextLine();
-        System.out.println("Escolha entre as opções:");
-        System.out.println("1.Docente");
-        System.out.println("2.Bolseiro");
-        int opcao = scanner.nextInt();
-        switch (opcao) {
-            case 1:
-                System.out.print("Numero mecanográfico: ");
-                int numMecanografico = scanner.nextInt();
-                System.out.print("Área Investigação: ");
-                String areaInvestigacao = scanner.nextLine();
-
-                Docente d = new Docente(nome, mail, numMecanografico, areaInvestigacao);
-                pessoas.add(d);
-                break;
-            case 2:
-                System.out.println("Data inicio: ");
-                String dataInicio = scanner.nextLine();
-                System.out.println("Data fim: ");
-                String dataFim = scanner.nextLine();
-
-                System.out.println("Escolha entre as opções:");
-                System.out.println("1.Licenciatura");
-                System.out.println("2.Mestrado");
-                System.out.println("3.Doutoramento");
-                int opcaoB = scanner.nextInt();
-
-                switch (opcaoB) {
-                    case 1:
-                        System.out.println("Adicione Orientador: ");
-                        // Lista de todos os docentes
-                        ArrayList<Docente> docentes = new ArrayList<>();
-
-                        // Imprimir lista de docentes para o utilizador escolher
-                        imprimirDocentes(docentes);
-
-                        // Escolher Orientador(da lista de docentes)
-                        int i = scanner.nextInt();
-
-                        // Criar lista orientadores
-                        ArrayList<Docente> orientadores = new ArrayList<>();
-
-                        // Associar Orientador a Bolseiro
-                        orientadores.add(docentes.get(i - 1));
-                        System.out.println("Adicionar outro Orientador?(Sim/Não)");
-                        String resp = scanner.nextLine();
-
-                        // Ciclo se quiser adicionar mais do que um orientador
-                        while (resp != "Não" || resp != "n" || resp != "Nao" || resp != "nao") {
-
-                            // Imprimir lista docentes
-                            imprimirDocentes(docentes);
-
-                            // Escolher orientador da lista de docentes
-                            i = scanner.nextInt();
-
-                            // Associar Orientador a Bolseiro
-                            orientadores.add(docentes.get(i - 1));
-
-                            System.out.println("Adicionar outro Orientador?(Sim/Não)");
-                            resp = scanner.nextLine();
-                        }
-                        Licenciado l = new Licenciado(nome, mail, dataInicio, dataFim, orientadores);
-                        pessoas.add(l);
-                        break;
-                    case 2:
-                        System.out.println("Adicione Orientador: ");
-                        // Lista de todos os docentes
-                        ArrayList<Docente> docentesm = new ArrayList<>();
-
-                        // Imprimir lista de docentes para o utilizador escolher
-                        imprimirDocentes(docentesm);
-
-                        // Escolher Orientador(da lista de docentes)
-                        int n = scanner.nextInt();
-
-                        // Criar lista orientadores
-                        ArrayList<Docente> orientadoresm = new ArrayList<>();
-
-                        // Associar Orientador a Bolseiro
-                        orientadoresm.add(docentesm.get(n - 1));
-                        System.out.println("Adicionar outro Orientador?(Sim/Não)");
-                        String respm = scanner.nextLine();
-
-                        // Ciclo se quiser adicionar mais do que um orientador
-                        while (respm != "Não" || respm != "n" || respm != "Nao" || respm != "nao") {
-
-                            // Imprimir lista docentes
-                            imprimirDocentes(docentesm);
-
-                            // Escolher orientador da lista de docentes
-                            n = scanner.nextInt();
-
-                            // Associar Orientador a Bolseiro
-                            orientadoresm.add(docentesm.get(n - 1));
-
-                            System.out.println("Adicionar outro Orientador?(Sim/Não)");
-                            respm = scanner.nextLine();
-                        }
-                        Mestre m = new Mestre(nome, mail, dataInicio, dataFim, orientadoresm);
-                        pessoas.add(m);
-                        break;
-                    case 3:
-                        Doutorado dr = new Doutorado(nome, mail, dataInicio, dataFim);
-                        pessoas.add(dr);
-                    default:
-                        System.out.println("Escolha uma opção existente.");
-                }
-        }
-    }
-
-    public void printPessoas() {
-        for (Pessoa p : pessoas) {
-            System.out.println(p);
-        }
-    }
-
-    public void imprimirDocentes(ArrayList<Docente> docentes) {
-
-        /**
-         * Method to print list of all People who are Docente so user can choose his orientor.
-         * @param ArrayList of docentes
-         */
-
-        // Imprimir lista de todos os docentes para o utilizador escolher
-        for (Pessoa p : pessoas) {
-            // Ver se é Docente ou Bolseiro
-            if (p.getNumeroMecanografico() > 0) {
-                int i = 1;
-                // Imprimir todos os docentes
-                System.out.println(i + "." + p.getNome());
-                Docente dc = (Docente) p;
-                docentes.add(dc);
-                i++;
-            }
-        }
-    }
 }
-
 
